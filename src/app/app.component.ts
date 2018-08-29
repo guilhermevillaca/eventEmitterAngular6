bde58260949e893b51f4ddad03a0df0642beb01d
Guilherme Villaca <guidvillaca@gmail.com>
2018-08-29 11:56:32 -0300

    add ao componente principal o subscribe do alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AppEventService } from './app-event.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ee';
+  alertar: boolean = false;
+  tipoAlerta: string;
+  navigate: string;
+  msg: string;
+  constructor(){
+    AppEventService.alert.subscribe(data => {
+      this.onAlertListener(data);    
+    });
+  }
+
+  onAlertListener(data){
+    this.alertar = true;
+    this.tipoAlerta = data.tipo;
+    this.navigate = data.navigate;
+    this.msg = data.msg;
+  }
+
 }
